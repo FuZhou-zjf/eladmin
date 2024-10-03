7bd15376399e82207aaa0877cfc1c086e4e71f61
FuZhou-zjf <385666842@qq.com>
2024-10-04 01:21:05 +0800

    测试商品。主要是测试一下添加菜单，添加完了测试一下功能

---
+++ eladmin-system/src/main/java/me/zhengjie/demo/domain/Item.java
@@ -0,0 +1,70 @@
+/*
+*  Copyright 2019-2020 Zheng Jie
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*  http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*/
+package me.zhengjie.demo.domain;
+
+import lombok.Data;
+import cn.hutool.core.bean.BeanUtil;
+import io.swagger.annotations.ApiModelProperty;
+import cn.hutool.core.bean.copier.CopyOptions;
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.sql.Timestamp;
+import java.math.BigDecimal;
+import java.io.Serializable;
+
+/**
+* @website https://eladmin.vip
+* @description /
+* @author laozhao
+* @date 2024-10-04
+**/
+@Entity
+@Data
+@Table(name="bus_item")
+public class Item implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "`id`")
+    @ApiModelProperty(value = "id")
+    private Integer id;
+
+    @Column(name = "`name`",nullable = false)
+    @NotBlank
+    @ApiModelProperty(value = "name")
+    private String name;
+
+    @Column(name = "`price`",nullable = false)
+    @NotNull
+    @ApiModelProperty(value = "price")
+    private BigDecimal price;
+
+    @Column(name = "`quantity`")
+    @ApiModelProperty(value = "quantity")
+    private Integer quantity;
+
+    @Column(name = "`created_at`")
+    @ApiModelProperty(value = "createdAt")
+    private Timestamp createdAt;
+
+    @Column(name = "`updated_at`")
+    @ApiModelProperty(value = "updatedAt")
+    private Timestamp updatedAt;
+
+    public void copy(Item source){
+        BeanUtil.copyProperties(source,this, CopyOptions.create().setIgnoreNullValue(true));
+    }
+}
---
+++ eladmin-system/src/main/java/me/zhengjie/demo/rest/ItemController.java
@@ -0,0 +1,90 @@
+/*
+*  Copyright 2019-2020 Zheng Jie
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*  http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*/
+package me.zhengjie.demo.rest;
+
+import me.zhengjie.annotation.Log;
+import me.zhengjie.demo.domain.Item;
+import me.zhengjie.demo.service.ItemService;
+import me.zhengjie.demo.service.dto.ItemQueryCriteria;
+import org.springframework.data.domain.Pageable;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import io.swagger.annotations.*;
+import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import me.zhengjie.utils.PageResult;
+import me.zhengjie.demo.service.dto.ItemDto;
+
+/**
+* @website https://eladmin.vip
+* @author laozhao
+* @date 2024-10-04
+**/
+@RestController
+@RequiredArgsConstructor
+@Api(tags = "测试商品管理")
+@RequestMapping("/api/item")
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @Log("导出数据")
+    @ApiOperation("导出数据")
+    @GetMapping(value = "/download")
+    @PreAuthorize("@el.check('item:list')")
+    public void exportItem(HttpServletResponse response, ItemQueryCriteria criteria) throws IOException {
+        itemService.download(itemService.queryAll(criteria), response);
+    }
+
+    @GetMapping
+    @Log("查询测试商品")
+    @ApiOperation("查询测试商品")
+    @PreAuthorize("@el.check('item:list')")
+    public ResponseEntity<PageResult<ItemDto>> queryItem(ItemQueryCriteria criteria, Pageable pageable){
+        return new ResponseEntity<>(itemService.queryAll(criteria,pageable),HttpStatus.OK);
+    }
+
+    @PostMapping
+    @Log("新增测试商品")
+    @ApiOperation("新增测试商品")
+    @PreAuthorize("@el.check('item:add')")
+    public ResponseEntity<Object> createItem(@Validated @RequestBody Item resources){
+        itemService.create(resources);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    @Log("修改测试商品")
+    @ApiOperation("修改测试商品")
+    @PreAuthorize("@el.check('item:edit')")
+    public ResponseEntity<Object> updateItem(@Validated @RequestBody Item resources){
+        itemService.update(resources);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping
+    @Log("删除测试商品")
+    @ApiOperation("删除测试商品")
+    @PreAuthorize("@el.check('item:del')")
+    public ResponseEntity<Object> deleteItem(@RequestBody Integer[] ids) {
+        itemService.deleteAll(ids);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
